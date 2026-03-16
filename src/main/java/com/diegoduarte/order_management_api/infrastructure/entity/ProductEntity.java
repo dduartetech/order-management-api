a5c5ab0c7931af2a8e1960861cb44758a43b56c4
Diego Duarte <diegocavalcante040307@gmail.com>
2026-03-16 15:50:31 -0300

    feat: add domain entities, repositories and enums

---
+++ src/main/java/com/diegoduarte/order_management_api/infrastructure/entity/ProductEntity.java
@@ -0,0 +1,38 @@
+package com.diegoduarte.order_management_api.infrastructure.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Builder
+@Table(name = "products")
+public class ProductEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "price")
+    private BigDecimal price;
+
+    @Column(name = "stock")
+    private int stock;
+
+    @CreationTimestamp
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+}
